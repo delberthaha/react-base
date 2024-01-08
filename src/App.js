0b011b3fcec6d3a890007f0b9726fe1fb692e676
delberthh <delbert.liu@silksoftware.com>
2024-01-08 17:50:10 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const subject = "React"
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Hello {subject}!
+        </p>
       </header>
     </div>
   );
